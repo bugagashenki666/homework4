621ff1799f8525f8fde869dfb613303a68c0afba
Konstantin <kasyanov_k@list.ru>
2019-11-27 09:26:46 +0300

    Added task231

--- task231.py
+++ task231.py
@@ -1,11 +1,29 @@
+def print_list(l):
+    s = ""
+    for i_ in l:
+        s += str(i_) + ' '
+    print(s)
+
+
+def solution(l, pos, num_):
+    l.append(0)
+    for i in range(len(l) - 1, pos - 1, -1):
+        l[i] = l[i - 1]
+    l[pos - 1] = num_
+    print_list(l)
+
+
+def solution2(l, pos, num_):
+    l.insert(pos - 1, num)
+    print_list(l)
+
+
+def solution3(l, pos, num_):
+    l1 = l[:pos - 1] + [num_] + l[pos - 1:]
+    print_list(l1)
+
+
 N = int(input())
 lst = [int(x) for x in input().split()]
 num, position = [int(x) for x in input().split()]
-if 0 <= position < N:
-    lst.insert(position, num)
-elif position >= N:
-    lst.append(num)
-    s = ""
-for i in lst:
-    s += str(i) + ' '
-print(s)
+solution2(lst, position, num)
